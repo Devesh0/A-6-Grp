7572a0885d0be1c429f7e1b2e37463332d268c25
Josh <bowlesj@uni.coventry.ac.uk>
2016-02-07 22:13:45 +0000

    some changes

--- RobotGame.py
+++ RobotGame.py
@@ -1,11 +1,12 @@
 import sys
+
 from tkinter import *
 
-class robotGameBaseClass():
+class userInterface():
 
     def __init__ (self, window):
         # Initializes the first window
-        # Might need to refactor. There's a lot of code here. 
+         
         self.window = window
         self.menuCanvas = Canvas(self.window, width=300, height=300, bg='white')
         self.menuCanvas.pack(fill=BOTH, expand=1)
@@ -21,44 +22,85 @@ class robotGameBaseClass():
 
         self.exitButton = Button(self.menuCanvas, text="Exit", command = lambda: self.window.destroy())
         self.exitButton.pack()
-        
+
+    def settings(self):
+        pass
 
     def loadGameArena(self):
         # Eventually this will have a new game/load game option prior to loading the actual game
         self.menuCanvas.pack_forget()
-        self.window.attributes("-fullscreen", True) # Automatically goes full-screen. 
-        self.menuCanvas = Canvas(self.window, width=10000, height=10000, bg='white')
-        self.menuCanvas.pack()
-        self.window.bind("<Escape>", self.minimizeScreen)
+        theGame = theRobotGame(self.window)
 
-##        self.robot = self.menuCanvas.create_rectangle(25, 25, 25+25, 25+25) # Placeholder rectangle
-##
-##        self.x1,y1,x2,y2 = canvas.coords(self.robot)
-##
-##        xspeed = 10.0
-##        yspeed = 5.0
-##
-##        self.menuCanvas.bind("<Right>", rightKey)
-##
-##        def rightKey(event, self.robot, ):
-##            canvas.coords(self.robot
-
-        # Focus on getting a moving robot
-        
+class theRobotGame():
 
     
-        
-        
-    def minimizeScreen(self, event):
-        self.window.attributes("-fullscreen", False) #Need to program this so it goes back to a decent resolution
+    
+    def __init__(self, window):
+        self.window = window
         self.window.geometry("1000x600+200+60")
-    def settings(self):
-        pass
-    
+        self.createMap()
+        self.robot = self.zone.create_rectangle(10, 10, 20+20, 20+20)
+        self.x1, self.y1, self.x2, self.y2 = self.zone.coords(self.robot)
+        self.robotLocation = 1
+        
+    def createMap(self):
+        self.zone = Canvas(self.window, width=1000, height=600, bg='white')
+        self.zone.pack(fill=BOTH, expand=1)
+        
+        #teleport1 = self.zone.create_line
+        self.zone.x_min = 0
+        self.zone.y_min = 0
+        self.zone.x_max = 991
+        self.zone.y_max = 600
+        self.keyboardMovement()
+
+    def keyboardMovement(self):
+        # Temporary movement for testing purposes
+        def rightKey(event):
+            self.x1, self.y1, self.x2, self.y2 = self.zone.coords(self.robot)
+
+            if self.x2 >= self.zone.x_max:
+                self.zone.coords(self.robot, self.x1, self.y1, self.x2, self.y2)
+            else:
+                self.zone.coords(self.robot, self.x1+10, self.y1, self.x2+10, self.y2)
+            
+            
+        def leftKey(event):
+            self.x1, self.y1, self.x2, self.y2 = self.zone.coords(self.robot)
+
+            if self.x1 <= self.zone.x_min:
+                self.zone.coords(self.robot, self.x1, self.y1, self.x2, self.y2)
+            else:
+                self.zone.coords(self.robot, self.x1-10, self.y1, self.x2-10, self.y2)
+            
+        def upKey(event):
+            self.x1, self.y1, self.x2, self.y2 = self.zone.coords(self.robot)
+
+            if self.y1 <= self.zone.y_min:
+                self.zone.coords(self.robot, self.x1, self.y1, self.x2, self.y2)
+            else:
+                self.zone.coords(self.robot, self.x1, self.y1-10, self.x2, self.y2-10)
+            
+        def downKey(event):
+            self.x1, self.y1, self.x2, self.y2 = self.zone.coords(self.robot)
+
+            if self.y2 >= self.zone.y_max:
+                self.zone.coords(self.robot, self.x1, self.y1, self.x2, self.y2)
+            else:
+                self.zone.coords(self.robot, self.x1, self.y1+10, self.x2, self.y2+10)
+
+
+        self.zone.focus_set()
+        self.zone.bind("<Right>", rightKey)
+        self.zone.bind("<Left>", leftKey)
+        self.zone.bind("<Up>", upKey)
+        self.zone.bind("<Down>", downKey)
+        
+
 def main():
     window = Tk()
     window.geometry("400x200+450+300") # Window needs to be centered on each PC screen 
-    game = robotGameBaseClass(window)
+    game = userInterface(window)
     window.mainloop()
 
 if __name__=='__main__':
